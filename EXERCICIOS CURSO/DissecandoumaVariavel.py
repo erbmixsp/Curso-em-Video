c31cdb301730afdb421102fd10361713189dd7a5
Renato <representomix@gmail.com>
2022-02-01 22:16:49 -0300

    Dissecando uma Variável

---
+++ EXERCICIOS CURSO/DissecandoumaVariavel.py
@@ -0,0 +1,10 @@
+a = input('Digite algo: ')
+print('O Tipo Primitivo desse valor é', type (a))
+print('Só tem espaços? ', a.isspace())
+print('É um Numero? ', a.isnumeric())
+print('É alfabetico? ', a.isalpha())
+print('É alfanumérico? ', a.isalnum())
+print('Esta em em maisculas?', a.isupper())
+print('Esta em minusculo? ', a.islower())
+print('Esta Capitalizada? ', a.istitle())
+
