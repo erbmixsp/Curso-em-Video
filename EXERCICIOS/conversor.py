01da56711b269cac317af67bb7b254edc2824e0d
Renato <representomix@gmail.com>
2022-02-04 22:30:21 -0300

    conversor

--- EXERCICIOS/conversor.py
+++ EXERCICIOS/conversor.py
@@ -1,26 +1,11 @@
 real = float(input('Qunto vc Tem em sua carteira? R$ '))
 print('-'*40)
-dolar =  real  / 5.33
-euro =   real  / 6.10
-yuan =   real  / 0.84
-print('Com R${:.2f} vc pode comprar US${:.2f} dolares'.format(real,dolar))
+dolar = real / 5.33
+euro = real / 6.10
+yuan = real / 0.84
+print('Com R${:.2f} vc pode comprar US${:.2f} dolares'.format(real, dolar))
 
-print('Ou R${:.2f} vc pode comprar €{:.2f} euros'.format(real,euro))
-print('Ou ainda com R${:.2f} vc pode comprar  ¥ {:.2f} yuan'.format(real,yuan))
+print('Ou R${:.2f} vc pode comprar €{:.2f} euros'.format(real, euro))
+print(
+    'Ou ainda com R${:.2f} vc pode comprar  ¥ {:.2f} yuan'.format(real, yuan))
 print('-'*40)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
