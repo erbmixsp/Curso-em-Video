f05d3e8e44269a1a5530c4385703ebed04824e11
Renato <representomix@gmail.com>
2022-02-06 19:18:11 -0300

    percentual

--- EXERCICIOS/porcentagemexec12.py
+++ EXERCICIOS/porcentagemexec12.py
@@ -1,4 +1,3 @@
 preco = float(input('Digite o Preço do Produto?R$ '))
 novo = preco - (preco * 5 /100)
 print('O Produto que custava R${:.2f} sai com 5% de desconto vai custar R${:.2f}'.format(preco, novo))
-
