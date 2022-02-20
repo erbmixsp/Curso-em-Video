9620090af01e37d316945fca9854250f204460c4
Renato <representomix@gmail.com>
2022-02-20 12:43:37 -0300

    Dissecando uma Variável

--- EXERCICIOS/exercicio04.py
+++ EXERCICIOS/exercicio04.py
@@ -1,4 +1,5 @@
 a = input('Digite algo: ')
+print('-'*40)
 print('O Tipo Primitivo desse valor é', type(a))
 print('Só tem espaços? ', a.isspace())
 print('É um Numero? ', a.isnumeric())
@@ -7,3 +8,5 @@ print('É alfanumérico? ', a.isalnum())
 print('Esta em em maisculas?', a.isupper())
 print('Esta em minusculo? ', a.islower())
 print('Esta Capitalizada? ', a.istitle())
+print('-'*40)
+
