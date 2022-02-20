4a86887c2cbcdcde9d54bf26396b908399ccf6d3
Renato <representomix@gmail.com>
2022-02-20 14:20:34 -0300

    Dobro, Triplo, Raiz Quadrada

--- EXERCICIOS/exercicio06.py
+++ EXERCICIOS/exercicio06.py
@@ -1,9 +1,11 @@
+print('='*25)
+
 num = int(input('Digite um Numero? '))
 print('O dobro de {} vale {} '.format(num, num * 2))
 print('O Triplo de {} vale {}'. format(num, num * 3))
 print('A raiz quadrada de {} vale {:.2f}'.format(num, num ** (1/2) ))
 
-
+print('='*25)
 
 
 
