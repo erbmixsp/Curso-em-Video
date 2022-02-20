7d13d7c07656db7f424a01b39b0a3452809820f0
Renato <representomix@gmail.com>
2022-02-20 14:30:20 -0300

    Conversor de Medidas

--- EXERCICIOS/exercicio08.py
+++ EXERCICIOS/exercicio08.py
@@ -1,7 +1,11 @@
+print('='*60)
+print('CONVERSOR DE MEDIDAS')
+
 medida = float(input('Uma distância em metros: '))
-print('A Medida de: {}m coresponde a {}KM e {:.0f}cm'.format(medida,(medida / 1000),(medida*100)))
+print('A Medida de: {:.0f}m coresponde a {}KM e {:.0f}cm'.format(medida,(medida / 1000),(medida*100)))
 
 
+print('='*60)
 
 
 
