6c90ea71675f16f1e287d558742d5fe682e12d14
Renato <representomix@gmail.com>
2022-02-20 16:19:51 -0300

    Reajuste Salarial

---
+++ EXERCICIOS/exercicio13.py
@@ -0,0 +1,4 @@
+print('Abono Salarial de 10%')
+salario = float(input('Digite seu Salario Atual? '))
+aumento = salario + (salario * 10/100)
+print('Seu salario é {} e agora com 10%  de aumento passa a ser {} será {}'.format(salario, aumento,salario))
