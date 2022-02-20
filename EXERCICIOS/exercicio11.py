424b88c081a7a498caee2056577e2ce0e37ccbc5
Renato <representomix@gmail.com>
2022-02-20 15:03:12 -0300

    Pintando Parede e cores no terminal

--- EXERCICIOS/exercicio11.py
+++ EXERCICIOS/exercicio11.py
@@ -1,3 +1,6 @@
+print('\033[31;43mCalculo Para Pintar uma Parede\033[m')
+print('='*60)
+
 larg = float(input('Digite a Largura:'))
 alt = float(input('Digite a Largura: '))
 area = larg*alt
@@ -5,3 +8,7 @@ print('Sua Parede tem a Dimensão de: {}m² x {}m² e Sua area é de {}m²'.form
     alt, larg, area))
 tinta = area / 2
 print('Para pintar essa parede, vc precisará de: {}L de tinta'.format(tinta))
+print('='*60)
+print('\033[31;43mFIM\033[m')
+
+
